20960782d0990199c04b4832a7fa9c3b7ef23200
patrickrweber <patrickrenanweber@gmail.com>
2022-05-08 22:00:18 -0300

    desafio 03 parcial

---
+++ testeDesafio03/Desafio03.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace testeDesafio03
+{
+    class Desafio03
+    {
+
+        static void Main(string[] args)
+        {
+            Metodos metodo = new Metodos();
+            int n = metodo.CapturarN();
+            List<int> numeros = new List<int>();
+            numeros = metodo.CapturarVetor(numeros);
+            numeros.Sort();
+            numeros.Reverse();
+            int nTemporario = n;
+            List<int> numerosUsados;
+            numerosUsados = metodo.SomaNumeros(ref nTemporario, numeros);
+            numerosUsados.Sort();
+            metodo.MostrarUsados(nTemporario, numerosUsados);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ testeDesafio03/Metodos.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testeDesafio03
+{
+    class Metodos
+    {
+        public int CapturarN()
+        {
+            Console.WriteLine("Digite o número N: ");
+            return int.Parse(Console.ReadLine());
+        }
+        public List<int> CapturarVetor(List<int> numeros)
+        {
+            int qtdVetor = 0;
+            string opcao;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Defina o  " + (qtdVetor + 1) + "º número do vetor:");
+                numeros.Add(int.Parse(Console.ReadLine()));
+                Console.Clear();
+                Console.Write("Vetor: ");
+                foreach (int numero in numeros)
+                {
+                    Console.Write("[" + numero + "] ");
+                }
+                qtdVetor++;
+                Console.WriteLine();
+                Console.WriteLine("Deseja adicionar mais um número no vetor? S/N");
+                opcao = Console.ReadLine();
+            } while (opcao == "s" || opcao == "S");
+            return numeros;
+        }
+        public List<int> SomaNumeros(ref int nTemporario, List<int> numeros)
+        {
+            List<int> numerosUsados = new List<int>();
+            int qtdNumerosUsados1 = 0;
+            foreach (int numero in numeros)
+            {
+                while (nTemporario >= numero)
+                {
+                    nTemporario -= numero;
+                    numerosUsados.Add(numero);
+                    qtdNumerosUsados1++;
+                }
+
+            }
+            return numerosUsados;
+        }
+        public void MostrarUsados(int nTemporario, List<int> numerosUsados)
+        {
+            Console.Clear();
+            if (nTemporario == 0)
+            {
+                Console.Write("Numeros Usados:");
+                foreach (int usado in numerosUsados)
+                {
+                    Console.Write("[" + usado + "]");
+                }
+            }
+        }
+       
+    }
+}
